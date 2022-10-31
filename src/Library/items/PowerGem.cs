49d2adccb0c519dfee9836cae8c113e5bdbbf0a2
florencia2003 <maria.correaj@correo.ucu.edu.uy>
2022-10-31 09:52:22 -0300

    Gems

---
+++ src/Library/items/PowerGem.cs
@@ -0,0 +1,14 @@
+namespace RoleplayGame.Items
+{
+    public class PowerGem : IAttackItem
+    {
+        public int AttackPower
+        {
+            get
+            {
+                return 100;
+            }
+        }
+
+    }
+}
---
+++ src/Library/items/RealityGem.cs
@@ -0,0 +1,14 @@
+namespace RoleplayGame.Items
+{
+    public class RealityGem : IAttackItem
+    {
+        public int AttackPower
+        {
+            get
+            {
+                return 100;
+            }
+        }
+
+    }
+}
